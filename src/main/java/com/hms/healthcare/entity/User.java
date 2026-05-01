e5698cfca9759dae1bb2fe6a759983bc0b7326c1
NAGAJOTHI-A <anagajothi1372004@gmail.com>
2026-05-01 11:08:45 +0530

    2.Admin Account Creation Done

--- src/main/java/com/hms/healthcare/entity/User.java
+++ src/main/java/com/hms/healthcare/entity/User.java
@@ -1,5 +1,40 @@
 package com.hms.healthcare.entity;
 
+import java.time.LocalDateTime;
+
+import com.hms.healthcare.enums.HospitalRoles;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Long id;
+	@Column(nullable=false)
+	private String username;
+	@Column(nullable=false,unique=true)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false, unique=true)
+	private Long mobile;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private HospitalRoles role;
+	@Column(nullable = false)
+	private Boolean isActive;  
+	@Column(nullable = false,updatable = false)
+	private LocalDateTime createdAt;
+	
+	
 }
